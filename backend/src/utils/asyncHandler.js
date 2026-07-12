aa0ca349beeea97a4ec07c9c8a0e3a8e02df0e9c
prigo003 <230210116036@gtu.edu.in>
2026-07-12 14:14:34 +0530

    remove backend and frontend folders from repository

--- backend/src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (controllerFunction) => {
-  return (req, res, next) => {
-    Promise.resolve(controllerFunction(req, res, next)).catch(next);
-  };
-};
-
-module.exports = asyncHandler;
