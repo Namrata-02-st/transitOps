7b0c06419a79efcbf1a6b93997e591c252d54b93
NAMRATABA GOHIL <namratagohil02@gmail.com>
2026-07-12 16:38:50 +0530

    Add vehicle service functions for CRUD operations

---
+++ frontend/src/services/vehicleService.js
@@ -0,0 +1,8 @@
+import api from './api';
+
+export const getVehicles = (params = {}) => api.get('/vehicles', { params });
+export const getVehicle = (id) => api.get(`/vehicles/${id}`);
+export const createVehicle = (data) => api.post('/vehicles', data);
+export const updateVehicle = (id, data) => api.put(`/vehicles/${id}`, data);
+export const deleteVehicle = (id) => api.delete(`/vehicles/${id}`);
+export const retireVehicle = (id) => api.patch(`/vehicles/${id}/retire`);
